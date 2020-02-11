c1e04a39682fa00bf1223f26e079f1afdcef3ae1
sathya <sathya.vakacharla@gmail.com>
2020-02-11 20:37:44 +0530

    sitename changed, global css mapped for entire application

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Contentful site by Sathya`,
+    description: `Basic gatsby site using contentful`,
+    author: `Sathya`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
